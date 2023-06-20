2ebea2d9be7d5921f1b974bc1dfa44866e55af34
Ean Jason Velayo <ecvelayo@gmail.com>
2023-06-20 19:57:03 +0800

    Axios sampling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import './App.css';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HelloWorld from './views/HelloWorld/HelloWorld';
-import Card from './components/Card/Card';
-import { useParams } from 'react-router-dom';
-import NamesList from './components/NamesList/NamesList';
+import "./App.css";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 const App = () => {
   const route = useParams();
-    return (
-      <div className="App container p-4">
-          <div>
-            <h1>weird div</h1>
-            <h1>{route.name}</h1>
-          </div>
-          <Card message="Message" title="TEST"></Card>
-          <NamesList/>
-      </div>
-    );
+  const [task, setTask] = useState({});
+
+  const getTask = async () => {
+    const randomNumber = Math.floor(Math.random() * 100);
+    const test = await axios
+      .get(`https://jsonplaceholder.typicode.com/todos/${randomNumber}`)
+      .then((res) => {
+        setTask(res.data);
+        return res.data;
+      });
+    console.log(test)
   }
+
+  return (
+    <div className="App container p-4">
+      <div>
+        <h1>weird div</h1>
+        <h1>{route.name}</h1>
+        <img src="https://picsum.photos/200/300" alt="random"></img>
+        <h1>{task.title}</h1>
+        <button onClick={() => getTask()}>Get Task</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
