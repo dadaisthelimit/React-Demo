0dd40bb9216d57fd752fcbe3abb4eb6df34124ff
Ean Jason Velayo <ecvelayo@gmail.com>
2023-06-20 20:13:06 +0800

    useEffect with axios

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const App = () => {
   const route = useParams();
   const [task, setTask] = useState({});
-
   const getTask = async () => {
     const randomNumber = Math.floor(Math.random() * 100);
-    const test = await axios
+    await axios
       .get(`https://jsonplaceholder.typicode.com/todos/${randomNumber}`)
       .then((res) => {
         setTask(res.data);
         return res.data;
       });
-    console.log(test)
   }
+
+  useEffect(() => {
+    getTask();
+  }, {})
+
 
   return (
     <div className="App container p-4">
@@ -26,7 +29,6 @@ const App = () => {
         <h1>{route.name}</h1>
         <img src="https://picsum.photos/200/300" alt="random"></img>
         <h1>{task.title}</h1>
-        <button onClick={() => getTask()}>Get Task</button>
       </div>
     </div>
   );
